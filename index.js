346f1baa8e26d43bada2552a5dd39d6085625754
rahulnj~ <rahuljayaraman8@gmail.com>
2022-07-16 17:30:55 +0530

    added javascript.

---
+++ index.js
@@ -0,0 +1,21 @@
+let array = [
+  { name: 'sajeesh', age: 16 },
+  { name: 'rahul', age: 20 },
+  { name: 'rashik', age: 20 },
+]
+// console.log(array)
+
+// for (let i = 0; i < array.length; i++) {
+//   if (array[i].age === 20) {
+//     console.log(array[i])
+//   }
+// }
+
+// array.map((i) => console.log(i))
+// array.forEach((i) => console.log(i))
+
+function testFn(value) {
+  console.log(value)
+}
+
+testFn('sajeesh')
